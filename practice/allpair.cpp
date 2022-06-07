4a413b4ac0ad83032527544afa7a82d36add3d18
Jitesh117 <sahookumarjitesh@gmail.com>
2022-06-07 14:16:40 +0530

    added one more folder

---
+++ practice/allpair.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define node 6
+#define inf 999
+int costmatrix[node][node] =
+    {
+        {0, 3, 7, inf, 20, 6},
+        {inf, 0, inf, inf, inf, 2},
+        {inf, inf, 0, 8, 6, inf},
+        {inf, inf, inf, 0, 4, inf},
+        {inf, inf, inf, inf, 0, inf},
+        {inf, inf, inf, 3, inf, 0}};
+void solve()
+{
+    int cost[node][node];
+    int n = 6;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cost[i][j] = costmatrix[i][j];
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                if (cost[i][k] + cost[k][j] < cost[i][j])
+                    cost[i][j] = cost[i][k] + cost[k][j];
+            }
+    cout << "The matrix for shortes paths is:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << setw(5) << cost[i][j];
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+// #ifndef ONLINE_JUDGE
+
+//     freopen("input.txt", "r", stdin);
+
+//     freopen("output.txt", "w", stdout);
+
+// #endif // ONLINE_JUDGE
+    solve();
+
+    return 0;
+}
